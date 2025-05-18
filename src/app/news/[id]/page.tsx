71f5a3cd9a261003366116391c1259bf7dc6de64
alvorie <123foggy123@gmail.com>
2025-05-18 15:54:22 +0300

    fix: fix caching

--- src/app/news/[id]/page.tsx
+++ src/app/news/[id]/page.tsx
@@ -10,6 +10,8 @@ export default async function NewsPage(props: Props) {
   const params = await props.params;
   const { id } = params;
 
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
   // проверка id
   const news = newsData.find((item) => item.id.toString() === id);
 
--- src/app/random/page.tsx
+++ src/app/random/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { newsData } from "@/lib/newsData";
 import NewsDetail from "@/components/NewsDetail/NewsDetail";
 import { notFound } from "next/navigation";
@@ -6,7 +8,7 @@ export default async function RandomNewsPage() {
   const randomIndex = Math.floor(Math.random() * newsData.length);
   const randomNews = newsData[randomIndex];
 
-  await new Promise((resolve) => setTimeout(resolve, 800));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   if (!randomNews) {
     notFound();
